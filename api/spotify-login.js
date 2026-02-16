7a777c7218df9d1e56b5b1a651d0313373283196
Bianca <130195861+bianca-8@users.noreply.github.com>
2026-02-15 23:10:55 -0500

    add offline access

--- api/spotify-login.js
+++ api/spotify-login.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  const scope = "playlist-modify-public";
+  const scope = "playlist-modify-public offline_access";
   const redirect_uri = process.env.SPOTIFY_REDIRECT_URI;
 
   const authURL =
